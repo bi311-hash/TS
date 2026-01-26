49cfa397a6cfd21b810c77656dcc767dedc0f8fa
bi311-hash <biburanabhat756311@gmail.com>
2026-01-26 19:16:59 +0545

    removed unnessecary annotations

---
+++ src/assignment2.ts
@@ -0,0 +1,7 @@
+let count = 10; // ts infer that count is number because it sees 0. Explicity writing count as a number is not neccessary.
+let name = "B";
+let isAdmin = false;
+
+console.log(count) 
+console.log(name)
+console.log(isAdmin)
