1ab279c3190118b5efc57050194902933ecb41e9
bi311-hash <biburanabhat756311@gmail.com>
2026-01-28 15:19:48 +0545

    completed

--- src/assignment6.ts
+++ src/assignment6.ts
@@ -1,7 +1,23 @@
-interface Person {
-    name: "string";
-    address: "string";
-    age: "number";
-    email: "string";
-    
-}
+// // Create a Person interface with properties like Name, address, age, email, etc.
+// interface Person {
+//     name: string;
+//     address: string;
+//     age: number;
+//     email: string;
+// }
+// // Extend the Person interface to create a Student interface that includes additional properties like studentID, and yearOfStudy.
+// interface Student extends Person {
+//     studentID: string;
+//     Year: number;
+// }
+
+// // Extend the Person interface to create a Teacher interface that includes additional properties like employeeID, subjectSpecialization, and yearsOfExperience.
+// interface Teacher extends Person {
+//     employeeID: string;
+//     Subject: string;
+//     Experience: number;
+// }
+
+// // const printPersonDetails = (person: Student | Teacher) => {
+//     console.log("Name:" + )
+// }
